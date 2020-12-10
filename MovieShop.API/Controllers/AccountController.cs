e96dd41648976a3cb63f054aa8fa37e9787a9379
gul055 <33allendelon@Gmail.com>
2020-12-10 13:03:00 -0800

    12/10/2020 ClassMaterialsAddToken

--- MovieShop.API/Controllers/AccountController.cs
+++ MovieShop.API/Controllers/AccountController.cs
@@ -1,10 +1,16 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Tokens;
 using MovieShop.Core.Models.Request;
+using MovieShop.Core.Models.Response;
 using MovieShop.Core.ServiceInterfaces;
 using System;
 using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
 using System.Linq;
+using System.Security.Claims;
+using System.Text;
 using System.Threading.Tasks;
 
 namespace MovieShop.API.Controllers
@@ -14,10 +20,12 @@ namespace MovieShop.API.Controllers
     public class AccountController : ControllerBase
     {
         private readonly IUserService _userService;
+        private readonly IConfiguration _configuration;
 
-        public AccountController(IUserService userService)
+        public AccountController(IUserService userService, IConfiguration configuration)
         {
             _userService = userService;
+            _configuration = configuration;
         }
 
         [HttpPost]
@@ -46,9 +54,39 @@ namespace MovieShop.API.Controllers
             var user = await _userService.ValidateUser(loginRequest.Email, loginRequest.Password);
             if (user == null) return Unauthorized();
 
-            return Ok(user);
+            var userToken = GenerateJWT(user);
+            return Ok(new { userToken });
         }
 
+        private string GenerateJWT(UserLoginResponseModel userLoginResponseModel)
+        {
+            var claims = new List<Claim> {
+                new Claim(ClaimTypes.NameIdentifier, userLoginResponseModel.Id.ToString()),
+                new Claim(JwtRegisteredClaimNames.GivenName, userLoginResponseModel.FirstName),
+                new Claim(JwtRegisteredClaimNames.FamilyName, userLoginResponseModel.LastName),
+                new Claim(JwtRegisteredClaimNames.Email, userLoginResponseModel.Email)
+            };
 
+            var identityClaims = new ClaimsIdentity();
+            identityClaims.AddClaims(claims);
+
+            var securityKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_configuration["TokenSettings:PrivateKey"]));
+            var credentials = new SigningCredentials(securityKey, SecurityAlgorithms.HmacSha256Signature);
+            var expires = DateTime.UtcNow.AddHours(_configuration.GetValue<double>("TokenSettings:ExpirationHours"));
+
+            var tokenDescriptor = new SecurityTokenDescriptor
+            {
+                Subject = identityClaims,
+                Audience = _configuration["TokenSettings:Audience"],
+                Issuer = _configuration["TokenSettings:Issuer"],
+                SigningCredentials = credentials,
+                Expires = expires
+            };
+
+            var tokenHandler = new JwtSecurityTokenHandler();
+            var encodedToken = tokenHandler.CreateToken(tokenDescriptor);
+
+            return tokenHandler.WriteToken(encodedToken);
+        }
     }
 }
